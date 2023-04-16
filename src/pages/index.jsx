56617f028b7e65e6ea6ce57f45308c5bf4e64d14
BAGUS BUDI SATOTO <87259393+bagussatoto@users.noreply.github.com>
2023-04-16 12:23:46 +0700

    main program

---
+++ src/pages/index.jsx
@@ -0,0 +1,31 @@
+import HomepagePages from './homepage/Homepage';
+import AplikasiKalori from './aplikasi-kalkulator-kalori/AplikasiKalori';
+import AplikasiBMI from './aplikasi-kalkulator-bmi/AplikasiBMI';
+import TestPages from './homepage/test';
+import NotFound from './notfound/notfound';
+import AplkasiHidrasi from './aplikasi-kalkulator-hidrasi/AplikasiKalkulatorHidrasi';
+import AplikasiKebahagiaan from './aplikasi-kalkulator-kebahagiaan/AplikasiKebahagiaan';
+import AplikasiTidur from './aplikasi-kalkulator-siklus-tidur/AplikasiTidur';
+import AplikasiRokok from './aplikasi-kalkulator-rokok/AplikasiRokok';
+import AplikasiKafein from './aplikasi-kalkulator-kafein/AplikasiKafein';
+import AplikasiMeditasi from './aplikasi-meditasi/AplikasiMeditasi';
+import AplikasiVitamin from './aplikasi-vitamin/AplikasiVitamin';
+import AplikasiDepresi from './aplikasi-kalkulator-depresi/AplikasiDepresi';
+import Disclaimer from './disclaimer/Disclaimer';
+
+export {
+  HomepagePages,
+  TestPages,
+  AplikasiKalori,
+  AplikasiBMI,
+  NotFound,
+  AplkasiHidrasi,
+  AplikasiKebahagiaan,
+  AplikasiTidur,
+  AplikasiRokok,
+  AplikasiKafein,
+  AplikasiMeditasi,
+  AplikasiVitamin,
+  AplikasiDepresi,
+  Disclaimer,
+};
