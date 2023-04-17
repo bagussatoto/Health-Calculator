6cb819678d518191615fc922410a1fb31172bc69
BAGUS BUDI SATOTO <87259393+bagussatoto@users.noreply.github.com>
2023-04-17 07:47:41 +0700

    chore : ``assest``

---
+++ src/assets/index.jsx
@@ -0,0 +1,117 @@
+import Kalkulator from './icons/kalkulator.svg';
+import BMI from './icons/bmi.svg';
+import Burger from './icons/burger.svg';
+import Hidration from './icons/hidration.svg';
+import Medical from './icons/medical.svg';
+import Karbo from './icons/karbo.svg';
+import Lemak from './icons/lemak.svg';
+import Protein from './icons/protein.svg';
+import PieBeratBadan from './icons/pie-berat-badan.svg';
+import PieMasaOtot from './icons/pie-masa-otot.svg';
+import PieAtlit from './icons/pie-atlit.svg';
+import ParameterNormal from './icons/parameter-normal.svg';
+import ParameterKurus from './icons/parameter-kurus.svg';
+import ParameterObesitas from './icons/parameter-obesitas.svg';
+import MonoMan from './icons/mono-man.svg';
+import MonoWoman from './icons/mono-woman.svg';
+import ColorMan from './icons/color-man.svg';
+import ColorWoman from './icons/color-woman.svg';
+import Gelas from './icons/glass.svg';
+import Water from './icons/water.png';
+import HeroImg from './images/hero.svg';
+import EmojiOK from './icons/emoji-ok.svg';
+import EmojiNormal from './icons/emoji-normal.svg';
+import EmojiPuas from './icons/emoji-puas.svg';
+import EmojiSangatPuas from './icons/emoji-sangat-puas.svg';
+import EmojiTidakPuas from './icons/emoji-tidak-puas.svg';
+import EmojiSangatTidakPuas from './icons/emoji-sangat-tidak-puas.svg';
+import BedSleep from './icons/bed-sleep.svg';
+import WakeUp from './icons/wake-up.svg';
+import Smoke from './icons/smoke.svg';
+import kalender from './icons/kalender.svg';
+import orangMerokok from './icons/orangMerokok.svg';
+import kotak from './icons/kotak.svg';
+import rupiah from './icons/rupiah.svg';
+import Kopi from './icons/kopi.svg';
+import Teh from './icons/teh.svg';
+import Soda from './icons/soda.svg';
+import Energi from './icons/energi.svg';
+import TehHijau from './icons/teh-hijau.svg';
+import Expresso from './icons/expresso.svg';
+import Kafein from './icons/kafein.svg';
+import Relax from './icons/relax.svg';
+import BgPantai from './images/pantai.jpg';
+import SeaWave from './audio/sea-wave.mp3';
+import blobAnimation from './images/blobanimation.svg';
+import KapsulVitammin from './icons/kapsul-vitamin.svg';
+import DepresiRingan from './icons/depressi-ringan.svg';
+import DepresiSedang from './icons/depressi-sedang.svg';
+import DepresiBerat from './icons/depressi-berat.svg';
+import DepresiSangatBerat from './icons/depressi-sangat-berat.svg';
+import TidakDepresi from './icons/tidak-depressi.svg';
+import SadCry from './icons/sad-cry.svg';
+import forestAudio from './audio/forest.mp3';
+import sunriseAudio from './audio/sunrise.mp3';
+import forest from './images/forest.jpg';
+import sunrise from './images/sunrise.jpg';
+import pantai from './images/pantai.jpg';
+
+export {
+  HeroImg,
+  Kalkulator,
+  BMI,
+  Burger,
+  Hidration,
+  Karbo,
+  Lemak,
+  Protein,
+  PieBeratBadan,
+  PieMasaOtot,
+  PieAtlit,
+  ParameterNormal,
+  ParameterKurus,
+  ParameterObesitas,
+  MonoMan,
+  MonoWoman,
+  ColorMan,
+  ColorWoman,
+  Medical,
+  Gelas,
+  Water,
+  EmojiOK,
+  EmojiNormal,
+  EmojiSangatPuas,
+  EmojiPuas,
+  EmojiTidakPuas,
+  EmojiSangatTidakPuas,
+  BedSleep,
+  WakeUp,
+  Smoke,
+  kalender,
+  orangMerokok,
+  kotak,
+  rupiah,
+  Kopi,
+  Teh,
+  Soda,
+  Energi,
+  TehHijau,
+  Expresso,
+  Kafein,
+  Relax,
+  BgPantai,
+  SeaWave,
+  blobAnimation,
+  KapsulVitammin,
+  DepresiRingan,
+  DepresiSedang,
+  DepresiBerat,
+  DepresiSangatBerat,
+  TidakDepresi,
+  SadCry,
+  forestAudio,
+  sunriseAudio,
+  forest,
+  sunrise,
+  pantai,
+};
