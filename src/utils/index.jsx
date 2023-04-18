03b7062ea4175ed2ff5d060192ea728bdc57faf5
BAGUS BUDI SATOTO <87259393+bagussatoto@users.noreply.github.com>
2023-04-18 08:01:24 +0700

    chore : ``main program``

---
+++ src/utils/index.jsx
@@ -0,0 +1,11 @@
+import calories from './logic/Calories';
+import BMI from './logic/BMI';
+import KebutuhanAir from './logic/KebutuhanAir';
+import Rokok from './logic/Rokok';
+import Kebahagiaan from './logic/Kebahagiaan';
+import Tidur from './logic/Tidur';
+import RelaxTimer from './logic/RelaxTimer';
+import Depresi from './logic/Depresi';
+import generateQuotes from './logic/generateQuotes';
+
+export { calories, BMI, KebutuhanAir, Rokok, Kebahagiaan, Tidur, RelaxTimer, Depresi, generateQuotes };
