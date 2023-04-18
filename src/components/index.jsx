31ab7599a28bfc8c93b08be414cae4605297890f
BAGUS BUDI SATOTO <87259393+bagussatoto@users.noreply.github.com>
2023-04-18 08:09:52 +0700

    v

---
+++ src/components/index.jsx
@@ -0,0 +1,79 @@
+import Navbar from './navbar/Navbar';
+import Hero from './hero/Hero';
+import Tentang from './tentang/Tentang';
+import Layanan from './layanan/Layanan';
+import Button from './button/Button';
+import Kontak from './kontak/Kontak';
+import Footer from './footer/Footer';
+import Sidebar from './sidebar/Sidebar';
+import Gender from './gender/Gender';
+import ParameterTubuh from './parameter-tubuh/ParameterTubuh';
+import IntensitasAktivitas from './intensitas-aktivitas/IntensitasAktivitas';
+import Target from './target/Target';
+import TitleKalori from './title/TitleKalori';
+import TitleBMI from './title/TitleBMI';
+import TitleHidrasi from './title/TitleHidrasi';
+import FormKalkulatorKalori from './form/FormKalkulatorKalori';
+import ResultKalkulatorKalori from './result/ResultKalkulatorKalori';
+import FormKalkulatorBMI from './form/FormKalkulatorBMI';
+import ResultKalkulatorBMI from './result/ResultKalkulatorBMI';
+import FormKalkulatorHidrasi from './form/FormKalkulatorHidrasi';
+import ResultKalkulatorHidrasi from './result/ResultKalkulatorHidrasi';
+import FormKalkulatorKebahagiaan from './form/FormKalkulatorKebahagiaan';
+import TitleKebahagiaan from './title/TitleKebahagiaan';
+import ResultKalkulatorKebahagiaan from './result/ResultKalkulatorKebahagiaan';
+import FormKalkulatorTidur from './form/FormKalkulatorTidur';
+import TitleTidur from './title/TitleTidur';
+import ResultKalkulatorTidur from './result/ResultKalkulatorTidur';
+import FormKalkulatorRokok from './form/FormKalkulatorRokok';
+import FormKalkulatorKafein from './form/FormKalkulatorKafein';
+import TitleKafein from './title/TitleKafein';
+import FormMeditasi from './form/FormMeditasi';
+import TitleMeditasi from './title/TitleMeditasi';
+import TitleVitamin from './title/TitleVitamin';
+import FormKalkulatorVitamin from './form/FormKalkulatorVitamin';
+import ResultKalkulatorVitamin from './result/ResultKalkulatorVitamin';
+import FormKalkulatorDepresi from './form/FormKalkulatorDepresi';
+import TitleDepresi from './title/TitleDepresi';
+import ResultKalkulatorDepresi from './result/ResultKalkulatorDepresi';
+
+export {
+  Navbar,
+  Hero,
+  Tentang,
+  Layanan,
+  Button,
+  Kontak,
+  Footer,
+  Sidebar,
+  Gender,
+  ParameterTubuh,
+  IntensitasAktivitas,
+  Target,
+  FormKalkulatorKalori,
+  ResultKalkulatorKalori,
+  TitleKalori,
+  FormKalkulatorBMI,
+  TitleBMI,
+  ResultKalkulatorBMI,
+  ResultKalkulatorHidrasi,
+  FormKalkulatorHidrasi,
+  TitleHidrasi,
+  FormKalkulatorKebahagiaan,
+  TitleKebahagiaan,
+  ResultKalkulatorKebahagiaan,
+  FormKalkulatorTidur,
+  TitleTidur,
+  ResultKalkulatorTidur,
+  FormKalkulatorRokok,
+  FormKalkulatorKafein,
+  TitleKafein,
+  FormMeditasi,
+  TitleMeditasi,
+  TitleVitamin,
+  FormKalkulatorVitamin,
+  ResultKalkulatorVitamin,
+  FormKalkulatorDepresi,
+  TitleDepresi,
+  ResultKalkulatorDepresi,
+};
